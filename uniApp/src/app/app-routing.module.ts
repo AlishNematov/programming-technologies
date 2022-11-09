f1cefc4be21989bd8bddde017c38ebd4248d9ab6
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-09 21:22:12 +0500

    feat: add routing

--- uniApp/src/app/app-routing.module.ts
+++ uniApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdsComponent } from './pages/ads/ads.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PtComponent } from './pages/pt/pt.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'pt', component: PtComponent},
+  {path: 'ads', component: AdsComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
