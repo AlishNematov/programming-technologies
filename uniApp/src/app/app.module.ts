6949a805d60fe222becc82bfc3157d5f6a38680a
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-09 21:23:16 +0500

    feat: add components to root

--- uniApp/src/app/app.module.ts
+++ uniApp/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PtComponent } from './pages/pt/pt.component';
+import { AdsComponent } from './pages/ads/ads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PtComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
