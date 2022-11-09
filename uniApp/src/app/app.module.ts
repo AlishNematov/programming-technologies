7adf9a4b14ff619d4e4da286009ca508182ce5b4
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-09 21:35:43 +0500

    feat: add tabs to pt page

--- uniApp/src/app/app.module.ts
+++ uniApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatTabsModule} from '@angular/material/tabs';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +26,8 @@ import { AdsComponent } from './pages/ads/ads.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
