add213e94b03e18ed9e857ad04a4028742fb397a
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-09 20:33:57 +0500

    feat: add data for S3PTL1

---
+++ uniApp/src/assets/data/data.ts
@@ -0,0 +1,52 @@
+export const luggage = [
+  {
+    amountOfLuggage: 2,
+    totalWeight: 23,
+    id: 1
+  },
+  {
+    amountOfLuggage: 1,
+    totalWeight: 31,
+    id: 2
+  },
+  {
+    amountOfLuggage: 1,
+    totalWeight: 15,
+    id: 3
+  },
+  {
+    amountOfLuggage: 3,
+    totalWeight: 69,
+    id: 4
+  },
+  {
+    amountOfLuggage: 2,
+    totalWeight: 37,
+    id: 5
+  },
+  {
+    amountOfLuggage: 1,
+    totalWeight: 10,
+    id: 6
+  },
+  {
+    amountOfLuggage: 3,
+    totalWeight: 55,
+    id: 7
+  },
+  {
+    amountOfLuggage: 3,
+    totalWeight: 24,
+    id: 8
+  },
+  {
+    amountOfLuggage: 2,
+    totalWeight: 36,
+    id: 9
+  },
+  {
+    amountOfLuggage: 3,
+    totalWeight: 61,
+    id: 10
+  }
+]
